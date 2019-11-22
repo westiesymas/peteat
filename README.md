dea853d779f78ef062d8382b16a7bcd44e6ec1f8
django <westiesymas@gmail.com>
2019-11-22 19:54:01 +0100

    version 1.0
    version  vacia

--- README.md
+++
@@ -1,2 +0,0 @@
-# petEat
- petEat
--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-
-
 
 @NgModule({
   imports: [
@@ -17,13 +14,4 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {
-  bcresult: string;
-  constructor(private barcodeScanner: BarcodeScanner) { }
-  scan() {
-    this.barcodeScanner.scan().then(data => {
-        // this is called when a barcode is found
-        this.bcresult = data.text
-      });      
-  }
-}
+export class Tab2PageModule {}
