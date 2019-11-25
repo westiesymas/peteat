b96f3ee1082c45d4211d38776f7ae84d4490c750
django <westiesymas@gmail.com>
2019-11-25 18:38:07 +0100

    sql

---
+++ src/assets/seeds.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY, brand TEXT, product TEXT, valuation TEXT, ingredients TEXT);
+INSERT or IGNORE INTO product(id, brand, product, valuation, ingredientes) VALUES (11111111, "Acana", "Chicken", "Excellent", "Pollo");
+INSERT or IGNORE INTO product(id, brand, product, valuation, ingredientes) VALUES (11111112, "Acana", "Chicken", "Excellent", "Pollo");
+INSERT or IGNORE INTO product(id, brand, product, valuation, ingredientes) VALUES (11111113, "Acana", "Chicken", "Excellent", "Pollo");
+
+CREATE TABLE IF NOT EXISTS pets(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,breed TEXT,img TEXT);
+INSERT or IGNORE INTO developer VALUES (1, 'Simon', '', 'https://pbs.twimg.com/profile_images/858987821394210817/oMccbXv6_bigger.jpg');
+INSERT or IGNORE INTO developer VALUES (2, 'Max', '', 'https://pbs.twimg.com/profile_images/953978653624455170/j91_AYfd_400x400.jpg');
+INSERT or IGNORE INTO developer VALUES (3, 'Ben', '', 'https://pbs.twimg.com/profile_images/1060037170688417792/vZ7iAWXV_400x400.jpg');
